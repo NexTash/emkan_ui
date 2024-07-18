1f3f058aadd6bb6728d388aec08ec38238d84d15
mianshahid720 <mianshahid03206893720@gmail.com>
2024-07-18 23:00:47 +0500

    fixes

---
+++ emkan_ui/emkan_ui/doctype/material_issue_reason_code/material_issue_reason_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, nextash.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Material Issue Reason Code", {
+// 	refresh(frm) {
+
+// 	},
+// });
