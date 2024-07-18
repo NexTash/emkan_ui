a6a0844ce96a09657c4ecb9d435aae050460d75b
mianshahid720 <mianshahid03206893720@gmail.com>
2024-07-18 23:22:47 +0500

    fixes

--- emkan_ui/emkan_ui/doctype/material_issue_reason_code/material_issue_reason_code.js
+++ emkan_ui/emkan_ui/doctype/material_issue_reason_code/material_issue_reason_code.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, nextash.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Material Issue Reason Code", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Material Issue Reason Code", {
+	onload(frm) {
+        frm.set_query('account', () => {
+            return {
+                filters: {
+                    root_type: 'Expense'
+                }
+            }
+        })
+	},
+});
