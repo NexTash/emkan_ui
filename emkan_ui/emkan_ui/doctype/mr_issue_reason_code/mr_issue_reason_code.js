786e226af5046bf1b3ea82ac0c8c07b045b9703b
mianshahid720 <mianshahid03206893720@gmail.com>
2024-07-19 15:30:37 +0500

    fixes

---
+++ emkan_ui/emkan_ui/doctype/mr_issue_reason_code/mr_issue_reason_code.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, nextash.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("MR Issue Reason Code", {
+	// onload(frm) {
+    //     frm.set_query('account', () => {
+    //         return {
+    //             filters: {
+    //                 root_type: 'Expense'
+    //             }
+    //         }
+    //     })
+	// },
+});
--- emkan_ui/emkan_ui/doctype/mr_issue_reason_code/mr_issue_reason_code.py
+++ emkan_ui/emkan_ui/doctype/mr_issue_reason_code/mr_issue_reason_code.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class MaterialIssueReasonCode(Document):
+class MRIssueReasonCode(Document):
 	pass
