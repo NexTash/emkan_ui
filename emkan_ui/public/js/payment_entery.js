766999d240b1ba02b320aff9599408541baf421b
ahmedrazapk <ahmedrazapk>
2024-07-21 23:55:12 +0500

    fixes

--- emkan_ui/public/js/payment_entery.js
+++ emkan_ui/public/js/payment_entery.js
@@ -1,11 +1,11 @@
 frappe.ui.form.on("Payment Entry", {
     party_type: function(frm) {
         if (frm.doc.custom_advance_payment && frm.doc.party_type === "Supplier") {
-            frm.set_value('paid_to', 'Advance To Supplier - N');
+            frm.set_value('paid_to', 'Advance To Supplier - EE');
             frm.refresh();
         } 
         else if (frm.doc.custom_advance_payment && frm.doc.party_type === "Customer") {
-            frm.set_value('paid_from', 'Advance From Customer - N');
+            frm.set_value('paid_from', 'Advance From Customer - EE');
             frm.refresh();
         } 
     }
