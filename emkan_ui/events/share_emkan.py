e4e2476adbaf5c214a51b17174f3f30af7d7ee86
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-08-12 15:54:01 +0500

    fix

---
+++ emkan_ui/events/share_emkan.py
@@ -0,0 +1,17 @@
+import frappe
+
+
+def after_insert(doc, method=None):
+    if doc.share_doctype == "Material Request":
+        doc.read = 1
+        doc.write = 1
+
+    doc.save()
+    
+    frappe.db.commit()
+
+def remove_share(doc, method=None):
+    if doc.status == "Cancelled":
+        frappe.db.delete("DocShare", {"share_doctype": doc.reference_type, "share_name" : doc.reference_name, "user" : doc.allocated_to})    
+    frappe.db.commit()
+        
