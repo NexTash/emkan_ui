42b431647f9b2417f516aba49ae4b43bd4ee66d9
Mian Shahid <111737618+mianshahid720@users.noreply.github.com>
2024-08-14 19:52:13 +0500

    Update material_request.py

--- emkan_ui/events/material_request.py
+++ emkan_ui/events/material_request.py
@@ -2,16 +2,6 @@ import frappe
 from datetime import datetime
 from frappe.desk.form.assign_to import add
 
-def change_state(doc=None, method=None):
-    current_date = datetime.today()
-    schedule_date = datetime.strptime(str(doc.schedule_date), "%Y-%m-%d")
-    current_date = datetime.now()
-    if schedule_date <= current_date:
-        if doc.workflow_state == "Mgmt Approved":
-            frappe.db.set_value(doc.doctype, doc.name, 'workflow_state', 'Move to Purchase')
-    
-    frappe.db.commit()
-
 def assign_user(doc=None, method=None):
     assign_docs = frappe.get_all("Emkan Assignment Rule", {"doctypes": doc.doctype, "condition_key": ["!=", ""],"condition_value": ["!=", ""]}, ['name', 'condition_key', 'condition_value'])
     for row in assign_docs:
