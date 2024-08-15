1bf13f84f9ad0ab437bb6ef1aca04ce879ebc7df
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-08-16 00:27:53 +0500

    fix chnges

--- emkan_ui/public/js/purchase_order.js
+++ emkan_ui/public/js/purchase_order.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on("Purchase Order", {
     custom_department(frm, dt, dn){
-        if(frm.doc.custom_department == "BSI"){
+        if(frm.doc.custom_department == "EMKAN-4 (BSI)"){
             frappe.model.set_value(dt, dn, "custom_prefix", "BSI-")
         }
         else{
@@ -33,23 +33,4 @@ frappe.ui.form.on("Purchase Order", {
 			__("Get Items From")
 		);
     },
-	// validate: function(frm) {
-    //     // Check if there are any items in the Purchase Order
-    //     if (frm.doc.items && frm.doc.items.length > 0) {
-    //         let has_mr = false;
-    //         // Loop through the items to check if any of them has a linked Material Request
-    //         frm.doc.items.forEach(function(item) {
-    //             if (item.material_request) {
-    //                 has_mr = true;
-    //             }
-    //         });
-
-    //         if (!has_mr) {
-    //             // Throw an error if no Material Request is linked
-    //             frappe.msgprint(__('You cannot create a Purchase Order without a linked Material Request.'));
-    //             frappe.validated = false;
-    //         }
-    //     }
-    // }
-
 });
