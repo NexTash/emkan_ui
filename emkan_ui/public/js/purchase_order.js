5b16e92ee197f5b4436eb40efeb0504fca9aa411
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-08-16 12:37:48 +0500

    fix

--- emkan_ui/public/js/purchase_order.js
+++ emkan_ui/public/js/purchase_order.js
@@ -1,5 +1,13 @@
 frappe.ui.form.on("Purchase Order", {
-    custom_department(frm, dt, dn){
+    onload(frm, dt, dn){
+		if(frm.doc.custom_department == "EMKAN-4 (BSI)"){
+			frappe.model.set_value(dt, dn, "custom_prefix", "BSI-")
+		}
+		else{
+			frappe.model.set_value(dt, dn, "custom_prefix", "EE-S-")
+		}
+    },
+	custom_department(frm, dt, dn){
         if(frm.doc.custom_department == "EMKAN-4 (BSI)"){
             frappe.model.set_value(dt, dn, "custom_prefix", "BSI-")
         }
