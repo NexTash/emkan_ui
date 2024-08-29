b230cc3eec85ca8530d291aa8503e165bc75aaad
mianshahid720 <mianshahid03206893720@gmail.com>
2024-08-29 13:05:54 +0500

    assignment task completed

--- emkan_ui/events/material_request.py
+++ emkan_ui/events/material_request.py
@@ -13,9 +13,11 @@ def change_state(doc=None, method=None):
     frappe.db.commit()
 
 def assign_user(doc=None, method=None):
-    assign_docs = frappe.get_all("Emkan Assignment Rule", {"doctypes": doc.doctype, "condition_key": ["!=", ""],"condition_value": ["!=", ""]}, ['name', 'condition_key', 'condition_value'])
+    doc = doc.as_dict()
+    assign_docs = frappe.get_all("Emkan Assignment Rule", {"doctypes": doc["doctype"]}, ['*'])
     for row in assign_docs:
-        if doc.get(row.condition_key) and doc.get(row.condition_key) == row.condition_value:        
+        my_condition =  eval(row['conditions'], {}, {"doc": doc})
+        if my_condition == True:
             assign_doc = frappe.get_doc("Emkan Assignment Rule", row.name)
             for child in assign_doc.emkan_assignment_rule_users:
                 if(frappe.db.exists("ToDo", {"status" : ["!=", "Cancelled"], "allocated_to": child.user, "reference_name" : doc.name, "reference_type" : doc.doctype})):
