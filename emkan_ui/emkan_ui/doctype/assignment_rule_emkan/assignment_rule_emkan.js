b8517d9a19dd70b886a9e3fdbbfd5276e3f12c3e
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-08-29 21:14:20 +0500

    fix

---
+++ emkan_ui/emkan_ui/doctype/assignment_rule_emkan/assignment_rule_emkan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, NexTash and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Assignment Rule Emkan", {
+// 	refresh(frm) {
+
+// 	},
+// });
