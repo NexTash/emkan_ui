2b39671124cfc0967ecd66dfc2d452b1826af186
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-08-29 12:16:22 +0500

    fix

--- emkan_ui/emkan_ui/doctype/emkan_assignment_rule/emkan_assignment_rule.js
+++ emkan_ui/emkan_ui/doctype/emkan_assignment_rule/emkan_assignment_rule.js
@@ -2,20 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Emkan Assignment Rule", {
-	doctypes: function(frm) {
-        frappe.model.with_doctype(frm.doc.doctypes, function () {
-			var options = $.map(frappe.get_meta(frm.doc.doctypes).fields, function (d) {
-				if (
-					d.fieldname &&
-					// d.fieldtype === "Select" &&
-					frappe.model.no_value_type.indexOf(d.fieldtype) === -1
-				) {
-					return d.fieldname;
-				}
-				return null;
-			});
-			frm.fields_dict["conditions"].grid.update_docfield_property("condition_key", "options", options);
-			frm.fields_dict["conditions"].grid.refresh_field("condition_key");
-		});
-    }
+	// doctypes: function(frm) {
+    //     frappe.model.with_doctype(frm.doc.doctypes, function () {
+	// 		var options = $.map(frappe.get_meta(frm.doc.doctypes).fields, function (d) {
+	// 			if (
+	// 				d.fieldname &&
+	// 				// d.fieldtype === "Select" &&
+	// 				frappe.model.no_value_type.indexOf(d.fieldtype) === -1
+	// 			) {
+	// 				return d.fieldname;
+	// 			}
+	// 			return null;
+	// 		});
+	// 		frm.fields_dict["conditions"].grid.update_docfield_property("condition_key", "options", options);
+	// 		frm.fields_dict["conditions"].grid.refresh_field("condition_key");
+	// 	});
+    // }
 });
