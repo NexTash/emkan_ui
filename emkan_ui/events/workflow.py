83621478d3273dbe44d4aeb8fa9967e7a634b163
mianshahid720 <mianshahid03206893720@gmail.com>
2024-09-11 20:28:26 +0500

    workflow task completed

---
+++ emkan_ui/events/workflow.py
@@ -0,0 +1,72 @@
+import frappe
+
+
+def store_data(doc, method=None):
+    doc.custom_current_workflow_state = doc.workflow_state
+    old_doc = doc.get_doc_before_save()
+
+    state_exists = False
+
+    for row in doc.custom_workflow_status:
+        if row.workflow_states == doc.workflow_state:
+            row.approved_by = frappe.session.user
+            state_exists = True
+            break
+
+    if doc.workflow_state == "MR Prepared":
+        doc.custom_workflow_status = []
+        return
+    
+    if not state_exists:
+        doc.append("custom_workflow_status", {
+            "workflow_states": old_doc.workflow_state,
+            "approved_by": frappe.session.user
+        })
+    else:
+        doc.append("custom_workflow_status", {
+            "workflow_states": doc.workflow_state,
+            "approved_by": frappe.session.user
+        })
+
+
+
+
+    #Server Script by MZH
+    # Ensure the custom workflow log field is initialized
+    if not doc.get('custom_workflow_log'):
+        doc.set('custom_workflow_log', "")
+
+    # Get the current date and time
+    timestamp = frappe.utils.now()
+
+    if old_doc:
+        workflow_entry = f"{timestamp} - {frappe.session.user} from {old_doc.workflow_state} to {doc.workflow_state}"
+    else:
+        workflow_entry = f"{timestamp} - {frappe.session.user} to {doc.workflow_state}"
+    # Retrieve the existing workflow log
+    current_log = doc.get('custom_workflow_log')
+
+    # Append the new entry to the existing log
+    if current_log:
+        updated_log = f"{current_log}\n{workflow_entry}"
+    else:
+        updated_log = workflow_entry
+
+    # Update the custom workflow log field
+    doc.set('custom_workflow_log', updated_log)
+
+
+
+def last_state(doc, method=None):
+    # if doc.workflow_state == "COO Approved"
+    doc.custom_current_workflow_state = doc.workflow_state
+    doc.append("custom_workflow_status", {
+            "workflow_states": "COO Approved",
+            "approved_by": frappe.session.user
+        })
+    old_doc = doc.get_doc_before_save()
+    current_log = doc.get('custom_workflow_log')
+    timestamp = frappe.utils.now()
+    workflow_entry = f"{timestamp} - {frappe.session.user} from {old_doc.workflow_state} to {doc.workflow_state}"
+    updated_log = f"{current_log}\n{workflow_entry}"
+    doc.set('custom_workflow_log', updated_log)
--- emkan_ui/public/js/material_request.js
+++
@@ -1,9 +0,0 @@
-frappe.ui.form.on("Material Request", {
-	validate(frm, dt, dn){
-		frm.add_child("custom_workflow_status", {
-            workflow_states : frm.doc.workflow_state,
-            approved_by : frappe.session.user
-        })
-        frm.refresh_field("custom_workflow_status")
-    },
-});
