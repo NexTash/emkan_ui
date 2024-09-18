6cb3b7d69b47090f88728fcc99703871c5d0cb45
mianshahid720 <mianshahid03206893720@gmail.com>
2024-09-18 13:50:56 +0500

    print format fixed

--- emkan_ui/events/workflow.py
+++ emkan_ui/events/workflow.py
@@ -1,85 +1,3 @@
-# import frappe
-
-
-# def store_data(doc, method=None):
-#     timestamp = frappe.utils.now()
-#     doc.custom_current_workflow_state = doc.workflow_state
-#     old_doc = doc.get_doc_before_save()
-
-#     state_exists = False
-
-#     for row in doc.custom_workflow_status:
-#         if row.workflow_states == doc.workflow_state:
-#             row.approved_by = frappe.session.user
-#             state_exists = True
-#             break
-
-#     if doc.workflow_state == "MR Prepared":
-#         doc.custom_workflow_status = []
-#         if not doc.get("__islocal"):
-#             current_log = doc.get('custom_workflow_log')
-#             my_log = f"{timestamp} - {frappe.session.user} from {old_doc.workflow_state} to MR Prepared"
-#             updated_log = f"{current_log}\n{my_log}"
-#             doc.set('custom_workflow_log', updated_log)
-#         return
-    
-#     if not state_exists:
-#         doc.append("custom_workflow_status", {
-#             "workflow_states": old_doc.workflow_state,
-#             "approved_by": frappe.session.user
-#         })
-#     else:
-#         doc.append("custom_workflow_status", {
-#             "workflow_states": doc.workflow_state,
-#             "approved_by": frappe.session.user
-#         })
-
-
-
-
-#     #Server Script by MZH
-#     # Ensure the custom workflow log field is initialized
-#     if not doc.get('custom_workflow_log'):
-#         doc.set('custom_workflow_log', "")
-
-#     # Get the current date and time
-
-#     if old_doc:
-#         workflow_entry = f"{timestamp} - {frappe.session.user} from {old_doc.workflow_state} to {doc.workflow_state}"
-#     else:
-#         workflow_entry = f"{timestamp} - {frappe.session.user} to {doc.workflow_state}"
-#     # Retrieve the existing workflow log
-#     current_log = doc.get('custom_workflow_log')
-
-#     # Append the new entry to the existing log
-#     if current_log:
-#         updated_log = f"{current_log}\n{workflow_entry}"
-#     else:
-#         updated_log = workflow_entry
-
-#     # Update the custom workflow log field
-#     doc.set('custom_workflow_log', updated_log)
-
-
-
-# def last_state(doc, method=None):
-#     # if doc.workflow_state == "COO Approved"
-#     doc.custom_current_workflow_state = doc.workflow_state
-#     doc.append("custom_workflow_status", {
-#             "workflow_states": "COO Approved",
-#             "approved_by": frappe.session.user
-#         })
-#     old_doc = doc.get_doc_before_save()
-#     current_log = doc.get('custom_workflow_log')
-#     timestamp = frappe.utils.now()
-#     workflow_entry = f"{timestamp} - {frappe.session.user} from {old_doc.workflow_state} to {doc.workflow_state}"
-#     updated_log = f"{current_log}\n{workflow_entry}"
-#     doc.set('custom_workflow_log', updated_log)
-
-
-
-
-
 import frappe
 from datetime import datetime
 
@@ -89,7 +7,7 @@ def store_data(doc, method=None):
     current_date = datetime.now().date()
     # Get the current workflow state and the previous state
     old_doc = doc.get_doc_before_save()
-    
+    user_doc = frappe.get_doc("User", frappe.session.user)
     # Proceed only if there is a change in workflow_state
     if old_doc and doc.workflow_state != old_doc.workflow_state:
         # Store the current workflow state in the custom field
@@ -100,6 +18,7 @@ def store_data(doc, method=None):
         for row in doc.custom_workflow_status:
             if row.workflow_states == doc.workflow_state:
                 row.approved_by = frappe.session.user
+                row.approved_by_name = user_doc.full_name
                 row.date = current_date
                 state_exists = True
                 break
@@ -120,12 +39,14 @@ def store_data(doc, method=None):
             doc.append("custom_workflow_status", {
                 "workflow_states": old_doc.workflow_state,
                 "approved_by": frappe.session.user,
+                "approved_by_name" : user_doc.full_name,
                 "date" : current_date
             })
         else:
             doc.append("custom_workflow_status", {
                 "workflow_states": doc.workflow_state,
                 "approved_by": frappe.session.user,
+                "approved_by_name" : user_doc.full_name,
                 "date" : current_date
             })
 
@@ -151,6 +72,7 @@ def store_data(doc, method=None):
 def last_state(doc, method=None):
     # Get the previous document
     old_doc = doc.get_doc_before_save()
+    user_doc = frappe.get_doc("User", frappe.session.user)
     current_date = datetime.now().date()
 
     # Proceed only if there is a change in workflow_state
@@ -162,6 +84,7 @@ def last_state(doc, method=None):
         doc.append("custom_workflow_status", {
             "workflow_states": "COO Approved",
             "approved_by": frappe.session.user,
+            "approved_by_name" : user_doc.full_name,
             "date" : current_date
         })
 
