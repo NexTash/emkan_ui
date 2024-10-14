b8c86ff6c11b1cc1893b0ea553f1c432f005a002
mianshahid720 <mianshahid03206893720@gmail.com>
2024-10-14 22:05:25 +0500

    tool completed

--- emkan_ui/emkan_ui/doctype/bulk_scheduled_depreciation_posting_tool/bulk_scheduled_depreciation_posting_tool.py
+++ emkan_ui/emkan_ui/doctype/bulk_scheduled_depreciation_posting_tool/bulk_scheduled_depreciation_posting_tool.py
@@ -2,19 +2,21 @@
 # For license information, please see license.txt
 
 import frappe
+from datetime import datetime
 from frappe.model.document import Document
 
-
 class BulkScheduledDepreciationPostingtool(Document):
-    def validate(self):
-        asset_doc = frappe.get_doc('Asset Depreciation Schedule', self.asset_depreciation_schedule)
-        # frappe.msgprint(f"{asset_doc}")
-        for row in asset_doc.depreciation_schedule:
-        	self.append("depreciation_schedule",{
+	@frappe.whitelist()
+	def get_depreciations(self):
+		today = datetime.now().date()
+		asset_doc = frappe.get_doc('Asset Depreciation Schedule', self.asset_depreciation_schedule)
+		for row in asset_doc.depreciation_schedule:
+			if row.schedule_date < today:
+				self.append("depreciation_schedule",{
 					"schedule_date":row.schedule_date,
 					"depreciation_amount":row.depreciation_amount,
 					"accumulated_depreciation_amount":row.accumulated_depreciation_amount,
-					"journal_entry":row.journal_entry
-			})
+					# "journal_entry":row.journal_entry
+				})
             
 
