67585992480921b52c3468575fa86abc8f8ca143
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-10-14 21:41:38 +0500

    fix

--- emkan_ui/emkan_ui/doctype/bulk_scheduled_depreciation_posting_tool/bulk_scheduled_depreciation_posting_tool.py
+++ emkan_ui/emkan_ui/doctype/bulk_scheduled_depreciation_posting_tool/bulk_scheduled_depreciation_posting_tool.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2024, NexTash and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BulkScheduledDepreciationPostingtool(Document):
-	pass
+    def validate(self):
+        asset_doc = frappe.get_doc('Asset Depreciation Schedule', self.asset_depreciation_schedule)
+        # frappe.msgprint(f"{asset_doc}")
+        for row in asset_doc.depreciation_schedule:
+        	self.append("depreciation_schedule",{
+					"schedule_date":row.schedule_date,
+					"depreciation_amount":row.depreciation_amount,
+					"accumulated_depreciation_amount":row.accumulated_depreciation_amount,
+					"journal_entry":row.journal_entry
+			})
+            
+
