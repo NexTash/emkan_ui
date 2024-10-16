32a3e130485a6b8b69446637bbbd782b5505cdc9
hafsa-rajpoot <hafsaamir863@gmail.com>
2024-10-16 21:33:24 +0500

    asset depreciation bulk posting tool fix

---
+++ emkan_ui/emkan_ui/doctype/asset_depreciation_bulk_posting_tool/asset_depreciation_bulk_posting_tool.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, NexTash and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Asset Depreciation Bulk Posting Tool", {
+	// refresh(frm) {
+	// },
+});
