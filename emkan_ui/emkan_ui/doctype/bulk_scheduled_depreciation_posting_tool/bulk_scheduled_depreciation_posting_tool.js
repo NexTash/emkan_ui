6e797a80e266f527197ec694201231869522df64
mianshahid720 <mianshahid03206893720@gmail.com>
2024-10-17 16:33:58 +0500

    previous tool removed

--- emkan_ui/emkan_ui/doctype/bulk_scheduled_depreciation_posting_tool/bulk_scheduled_depreciation_posting_tool.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, NexTash and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Bulk Scheduled Depreciation Posting tool", {
-// 	refresh(frm) {
-
-// 	},
-// });
