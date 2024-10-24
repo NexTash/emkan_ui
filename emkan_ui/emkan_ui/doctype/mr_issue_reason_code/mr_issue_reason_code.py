80fa563a714ead1bd300ecbc699e3b6dab1efe1a
mianshahid720 <mianshahid03206893720@gmail.com>
2024-10-24 12:20:38 +0500

    report show

--- emkan_ui/emkan_ui/doctype/mr_issue_reason_code/mr_issue_reason_code.py
+++ emkan_ui/emkan_ui/doctype/mr_issue_reason_code/mr_issue_reason_code.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, nextash.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MRIssueReasonCode(Document):
-	pass
+	def validate(self):
+		doc = frappe.get_doc("Report", "Emkan Accounts Payable")
+		doc.db_set("add_total_row", 1)
+		frappe.db.commit()
