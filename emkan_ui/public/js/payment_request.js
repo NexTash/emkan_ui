5d629506acc8c0b7a931206bda5d62f91544f235
alibajwa007 <babargoraya878@gmail.com>
2024-11-19 15:03:09 +0500

    fixes

--- emkan_ui/public/js/payment_request.js
+++ emkan_ui/public/js/payment_request.js
@@ -8,28 +8,39 @@ frappe.ui.form.on('Payment Request', {
 
 
 frappe.ui.form.on('Payment Request', {
-    party: function (frm) {
+    onload: function (frm) {
+        // Populate Party Name if Party Type and Party are already selected
         if (frm.doc.party_type && frm.doc.party) {
-            let doctype_map = {
-                'Customer': 'Customer',
-                'Supplier': 'Supplier',
-                'Employee': 'Employee',
-                'Shareholder': 'Shareholder'
-            };
-
-            let doctype = doctype_map[frm.doc.party_type];
-            if (doctype) {
-                frappe.db.get_value(doctype, frm.doc.party, doctype.toLowerCase() + '_name', (r) => {
-                    frm.set_value('party_name', r[doctype.toLowerCase() + '_name'] || r.name); // Fallback to name if *_name doesn't exist
-                });
-            }
-        } else {
-            frm.set_value('party_name', null);
+            fetch_party_name(frm);
         }
     },
+    party: function (frm) {
+        // Fetch Party Name when Party is selected
+        fetch_party_name(frm);
+    },
     party_type: function (frm) {
-        // Clear party_name and party when party type is changed
-        frm.set_value('party_name', null);
+        // Clear Party and Party Name when Party Type is changed
         frm.set_value('party', null);
+        frm.set_value('party_name', null);
     }
 });
+
+// Function to fetch Party Name based on Party Type and Party
+function fetch_party_name(frm) {
+    if (frm.doc.party_type && frm.doc.party) {
+        let doctype_map = {
+            'Customer': 'Customer',
+            'Supplier': 'Supplier',
+            'Employee': 'Employee',
+            'Shareholder': 'Shareholder'
+        };
+
+        let doctype = doctype_map[frm.doc.party_type];
+        if (doctype) {
+            frappe.db.get_value(doctype, frm.doc.party, doctype.toLowerCase() + '_name', (r) => {
+                frm.set_value('party_name', r[doctype.toLowerCase() + '_name'] || r.name); // Fallback to `name` if `*_name` doesn't exist
+            });
+        }
+    }
+}
+
