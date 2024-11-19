53371d1b35fb1565269525163aac70789070e6cf
alibajwa007 <babargoraya878@gmail.com>
2024-11-19 21:45:58 +0500

    fixes

--- emkan_ui/public/js/payment_request.js
+++ emkan_ui/public/js/payment_request.js
@@ -1,28 +1,28 @@
 frappe.ui.form.on('Payment Request', {
-    onload: function(frm) {
+    onload: function (frm) {
+        // Populate Party Name if Party Type and Party are already selected
+        // if (frm.doc.party_type && frm.doc.party) {
+            fetch_party_name(frm);
+        // }
         if (frm.doc.message && frm.doc.message.includes("Thank you for your business!")) {
             frm.set_value('message', ''); 
         }
-    }
-});
-
-
-frappe.ui.form.on('Payment Request', {
-    onload: function (frm) {
+    },
+    refresh: function (frm) {
         // Populate Party Name if Party Type and Party are already selected
-        if (frm.doc.party_type && frm.doc.party) {
+        // if (frm.doc.party_type && frm.doc.party) {
             fetch_party_name(frm);
-        }
+        // }
     },
     party: function (frm) {
         // Fetch Party Name when Party is selected
         fetch_party_name(frm);
     },
-    party_type: function (frm) {
-        // Clear Party and Party Name when Party Type is changed
-        frm.set_value('party', null);
-        frm.set_value('party_name', null);
-    }
+    // party_type: function (frm) {
+    //     // Clear Party and Party Name when Party Type is changed
+    //     frm.set_value('party', null);
+    //     frm.set_value('party_name', null);
+    // }
 });
 
 // Function to fetch Party Name based on Party Type and Party
