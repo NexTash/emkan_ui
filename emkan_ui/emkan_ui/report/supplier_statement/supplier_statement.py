9add36737dc7b8d61e2a6e16159e01220f169b4c
alibajwa007 <alibajwa007@gmail.com>
2024-12-09 14:32:56 +0500

    report payment entry Reconciliation fixes

--- emkan_ui/emkan_ui/report/supplier_statement/supplier_statement.py
+++ emkan_ui/emkan_ui/report/supplier_statement/supplier_statement.py
@@ -151,61 +151,71 @@ def get_result(filters, account_details):
 
 
 def get_gl_entries(filters, accounting_dimensions):
-	currency_map = get_currency(filters)
-	select_fields = """, debit, credit, debit_in_account_currency,
-		credit_in_account_currency """
+    currency_map = get_currency(filters)
+    select_fields = """, debit, credit, debit_in_account_currency,
+        credit_in_account_currency """
 
-	if filters.get("show_remarks"):
-		if remarks_length := frappe.db.get_single_value("Accounts Settings", "general_ledger_remarks_length"):
-			select_fields += f",substr(remarks, 1, {remarks_length}) as 'remarks'"
-		else:
-			select_fields += """,remarks"""
+    if filters.get("show_remarks"):
+        if remarks_length := frappe.db.get_single_value("Accounts Settings", "general_ledger_remarks_length"):
+            select_fields += f",substr(remarks, 1, {remarks_length}) as 'remarks'"
+        else:
+            select_fields += """,remarks"""
 
-	order_by_statement = "order by posting_date, account, creation"
+    order_by_statement = "order by posting_date, account, creation"
 
-	if filters.get("include_dimensions"):
-		order_by_statement = "order by posting_date, creation"
+    if filters.get("include_dimensions"):
+        order_by_statement = "order by posting_date, creation"
 
-	if filters.get("group_by") == "Group by Voucher":
-		order_by_statement = "order by posting_date, voucher_type, voucher_no"
-	if filters.get("group_by") == "Group by Account":
-		order_by_statement = "order by account, posting_date, creation"
+    if filters.get("group_by") == "Group by Voucher":
+        order_by_statement = "order by posting_date, voucher_type, voucher_no"
+    if filters.get("group_by") == "Group by Account":
+        order_by_statement = "order by account, posting_date, creation"
 
-	if filters.get("include_default_book_entries"):
-		filters["company_fb"] = frappe.get_cached_value(
-			"Company", filters.get("company"), "default_finance_book"
-		)
+    if filters.get("include_default_book_entries"):
+        filters["company_fb"] = frappe.get_cached_value(
+            "Company", filters.get("company"), "default_finance_book"
+        )
 
-	dimension_fields = ""
-	if accounting_dimensions:
-		dimension_fields = ", ".join(accounting_dimensions) + ","
+    dimension_fields = ""
+    if accounting_dimensions:
+        dimension_fields = ", ".join(accounting_dimensions) + ","
 
-	transaction_currency_fields = ""
-	if filters.get("add_values_in_transaction_currency"):
-		transaction_currency_fields = (
-			"debit_in_transaction_currency, credit_in_transaction_currency, transaction_currency,"
-		)
+    transaction_currency_fields = ""
+    if filters.get("add_values_in_transaction_currency"):
+        transaction_currency_fields = (
+            "debit_in_transaction_currency, credit_in_transaction_currency, transaction_currency,"
+        )
 
-	gl_entries = frappe.db.sql(
-		f"""
-		select
-			name as gl_entry, posting_date, account, party_type, party,
-			voucher_type, voucher_subtype, voucher_no, {dimension_fields}
-			cost_center, project, {transaction_currency_fields}
-			against_voucher_type, against_voucher, account_currency,
-			against, is_opening, creation {select_fields}
-		from `tabGL Entry`
-		where company=%(company)s {get_conditions(filters)}
-		{order_by_statement}
-	""",
-		filters,
-		as_dict=1,
-	)
+    # Fetch GL Entries
+    gl_entries = frappe.db.sql(
+        f"""
+        select
+            name as gl_entry, posting_date, account, party_type, party,
+            voucher_type, voucher_subtype, voucher_no, {dimension_fields}
+            cost_center, project, {transaction_currency_fields}
+            against_voucher_type, against_voucher, account_currency,
+            against, is_opening, creation {select_fields}
+        from `tabGL Entry`
+        where company=%(company)s {get_conditions(filters)}
+        {order_by_statement}
+        """,
+        filters,
+        as_dict=1,
+    )
 
-	if filters.get("presentation_currency"):
-		return convert_to_presentation_currency(gl_entries, currency_map)
-	else:
-		return gl_entries
+    # Filter out Payment Entry records that are reconciliations
+    filtered_gl_entries = []
+    for gle in gl_entries:
+	    if gle.get("voucher_type") == "Payment Entry":
+		    voucher_no = gle.get("voucher_no")
+		    if frappe.db.exists("Payment Entry Reference", {"parent": voucher_no}):
+			    continue  # Skip if Payment Entry is a reconciliation
+	    filtered_gl_entries.append(gle)
+
+    if filters.get("presentation_currency"):
+        return convert_to_presentation_currency(filtered_gl_entries, currency_map)
+    else:
+        return filtered_gl_entries
 
 
 def get_conditions(filters):
