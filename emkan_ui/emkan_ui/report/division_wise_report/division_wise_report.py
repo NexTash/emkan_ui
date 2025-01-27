de9f7e12dd86f62d935787b68ad1f8151a1a3362
alibajwa007 <alibajwa007@gmail.com>
2025-01-27 17:40:57 +0500

    Refactor:Devision Wise Report without project

---
+++ emkan_ui/emkan_ui/report/division_wise_report/division_wise_report.py
@@ -0,0 +1,62 @@
+import frappe
+from frappe.utils import flt
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    return [
+        {"label": "Division", "fieldname": "division", "fieldtype": "Data", "width": 200},
+        {"label": "Income", "fieldname": "total_income", "fieldtype": "Currency", "width": 200},
+        {"label": "Expense", "fieldname": "total_expense", "fieldtype": "Currency", "width": 200},
+        {"label": "Profit", "fieldname": "net_income_loss", "fieldtype": "Currency", "width": 200},
+    ]
+
+def get_data(filters):
+    conditions = "1=1"
+    
+    if filters.get("from_date") and filters.get("to_date"):
+        conditions += " AND gle.posting_date BETWEEN %(from_date)s AND %(to_date)s"
+    if filters.get("cost_center"):
+        conditions += " AND gle.cost_center = %(cost_center)s"
+    
+    query = f"""
+        SELECT 
+            gle.cost_center AS division,
+            SUM(CASE 
+                    WHEN acc.root_type = 'Income' THEN gle.credit - gle.debit
+                    ELSE 0
+                END) AS total_income,
+            SUM(CASE 
+                    WHEN acc.root_type = 'Expense' THEN gle.debit - gle.credit
+                    ELSE 0
+                END) AS total_expense,
+            SUM(CASE 
+                    WHEN acc.root_type = 'Income' THEN gle.credit - gle.debit
+                    ELSE 0
+                END) - 
+            SUM(CASE 
+                    WHEN acc.root_type = 'Expense' THEN gle.debit - gle.credit
+                    ELSE 0
+                END) AS net_income_loss
+        FROM 
+            `tabGL Entry` AS gle
+        INNER JOIN 
+            `tabAccount` AS acc ON gle.account = acc.name
+        WHERE 
+            acc.report_type = 'Profit and Loss'
+            AND {conditions}
+        GROUP BY
+            gle.cost_center,
+    """
+    
+    result = frappe.db.sql(query, filters, as_dict=True)
+    
+    filtered_result = [
+        row for row in result 
+        if not (flt(row["total_income"]) == 0 and flt(row["total_expense"]) == 0 and flt(row["net_income_loss"]) == 0)
+    ]
+    
+    return filtered_result
