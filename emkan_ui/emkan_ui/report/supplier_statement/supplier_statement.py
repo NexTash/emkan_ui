3c239388c8e577de017b81d0904f3c487459fd89
alibajwa007 <alibajwa007@gmail.com>
2025-02-06 13:26:40 +0500

    Refactor: Supplier Statement Report fix

--- emkan_ui/emkan_ui/report/supplier_statement/supplier_statement.py
+++ emkan_ui/emkan_ui/report/supplier_statement/supplier_statement.py
@@ -215,14 +215,6 @@ def get_gl_entries(filters, accounting_dimensions):
         if gle.get("is_canceled") == 1:
             continue
 
-        if gle.get("voucher_type") == "Payment Entry":
-            voucher_no = gle.get("voucher_no")
-
-            if voucher_no in processed_payment_entries:
-                continue
-
-            processed_payment_entries.add(voucher_no)
-
         filtered_gl_entries.append(gle)
 
     if filters.get("presentation_currency"):
@@ -724,3 +716,4 @@ def get_columns(filters):
 		columns.extend([{"label": _("Remarks"), "fieldname": "remarks", "width": 400}])
 
 	return columns
+
