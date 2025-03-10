744a595eddb888471f5030517c2987ffc4188297
alibajwa007 <babargoraya878@gmail.com>
2025-03-10 15:07:10 +0500

    Refactor: PO Linked Mr

--- emkan_ui/public/js/purchase_order.js
+++ emkan_ui/public/js/purchase_order.js
@@ -42,14 +42,14 @@ frappe.ui.form.on("Purchase Order", {
 		);
 
 
-		//  let mrSet = new Set();
+		 let mrSet = new Set();
 
-		//  frm.doc.items.forEach(function(item) {
-		//  	if (item.material_request) {
-		//  		mrSet.add(item.material_request);
-		//  	}
-		//  });
+		 frm.doc.items.forEach(function(item) {
+		 	if (item.material_request) {
+		 		mrSet.add(item.material_request);
+		 	}
+		 });
 
-		//  frm.doc.custom_linked_mr = Array.from(mrSet).join(', ');
+		 frm.doc.custom_linked_mr = Array.from(mrSet).join(', ');
     },
 });
