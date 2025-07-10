729058b04e43784624b731b6b9092861dbaf0c6a
alibajwa007 <babargoraya878@gmail.com>
2025-07-10 15:47:09 +0500

    Refactor: Leave Application Updated with Workflow Log

--- emkan_ui/events/leave_application.py
+++ emkan_ui/events/leave_application.py
@@ -6,21 +6,35 @@ def clear_child_table_on_creation(doc, method=None):
     if doc.__islocal:
         doc.custom_workflow_status = []
 
-def add_workflow_status(doc, method=None):
-    current_date = datetime.now().date()
-    user_doc = frappe.get_doc("User", frappe.session.user)
 
+def add_workflow_status(doc, method=None):
     old_doc = doc.get_doc_before_save()
 
-    if old_doc is None or (doc.workflow_state != old_doc.workflow_state):
-        doc.custom_current_workflow_state = doc.workflow_state
+    if not old_doc:
+        return
 
+    if doc.workflow_state != old_doc.workflow_state:
+        previous_state = old_doc.workflow_state
+        current_state = doc.workflow_state
+
+        if current_state == "Draft":
+            doc.custom_workflow_status = []
+            return 
+
+        if current_state == "Rejected":
+            return
+
+        user_doc = frappe.get_doc("User", frappe.session.user)
+        current_date = datetime.now().date()
+
+        doc.custom_current_workflow_state = current_state
         doc.append("custom_workflow_status", {
-            "workflow_states": doc.workflow_state,
+            "workflow_states": previous_state,
             "approved_by": frappe.session.user,
             "approved_by_name": user_doc.full_name,
             "date": current_date
         })
+
 
 def last_state(doc, method=None):
     old_doc = doc.get_doc_before_save()
