2f3f688731f0ae580d12bdaddc5ac5efc0851d51
alibajwa007 <babargoraya878@gmail.com>
2025-07-29 09:40:17 +0500

    Refactor: Vehicle Service Request

--- emkan_ui/emkan_ui/doctype/vehicle_service_request/test_vehicle_service_request.py
+++ emkan_ui/emkan_ui/doctype/vehicle_service_request/test_vehicle_service_request.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestVehicleServiceLog(FrappeTestCase):
+class TestVehicleServiceRequest(FrappeTestCase):
 	pass
--- emkan_ui/emkan_ui/doctype/vehicle_service_request/vehicle_service_request.js
+++ emkan_ui/emkan_ui/doctype/vehicle_service_request/vehicle_service_request.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, NexTash and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Vehicle Service Log", {
+frappe.ui.form.on("Vehicle Service Request", {
   refresh: function(frm) {
     // For Employee (Driver / Staff)
     frappe.call({
