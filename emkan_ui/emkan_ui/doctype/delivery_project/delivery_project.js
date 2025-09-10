64cc7046529b219039ac9a31b6be74f1113ed858
Rubab <rubabawan786>
2025-09-10 18:46:27 +0500

    Add Delivery Project doctype with initial fields and structure

---
+++ emkan_ui/emkan_ui/doctype/delivery_project/delivery_project.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, NexTash and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Delivery Project", {
+// 	refresh(frm) {
+
+// 	},
+// });
