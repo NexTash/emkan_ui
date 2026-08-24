a9ea6faa0d80b5f2ee2d4fb945e1f685114acb27
saqlain-zahid-6468 <saqlainzahid6468@gmail.com>
2026-08-24 15:01:55 +0500

    Remove unused import of get_party_bank_account from custom_payment_request.py

--- emkan_ui/emkan_ui/doctype/custom_payment_request/custom_payment_request.py
+++ emkan_ui/emkan_ui/doctype/custom_payment_request/custom_payment_request.py
@@ -15,7 +15,7 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import (
 	get_payment_entry,
 )
 from erpnext.accounts.doctype.subscription_plan.subscription_plan import get_plan_rate
-from erpnext.accounts.party import get_party_account, get_party_bank_account
+from erpnext.accounts.party import get_party_account
 from erpnext.accounts.utils import get_account_currency, get_currency_precision
 from erpnext.utilities import payment_app_import_guard
 
